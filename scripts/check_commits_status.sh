a0bb375522d730a733fe40a30d8f14e2e9ef3493
fengchunsong <chunsongfeng@163.com>
2017-02-03 17:42:02 +0800

    add merged file
    Signed-off-by: fengchunsong <chunsongfeng@163.com>

--- scripts/check_commits_status.sh
+++ scripts/check_commits_status.sh
@@ -44,13 +44,14 @@ while true; do
 	esac
 done
 
-MRGD_FILE=$OUT_DIR/mrgd-subject.txt
+MRGD_FILE=$OUT_DIR/mrgd.txt
 UMRGD_FILE=$OUT_DIR/umrgd.txt
 CHRY_PICKS=$OUT_DIR/umrgd-cherry-pick.sh
 CONFLICT_CMTS=$OUT_DIR/umrgd-conflict.txt
 TARGET_LOG=$OUT_DIR/.target.log
 UMRGD_CIDS=$OUT_DIR/.umrgd-cid.txt
 SORT_UMRGD_CIDS=$OUT_DIR/.umrgd-scid.txt
+MRGD_SUBJECT_FILE=$OUT_DIR/mrgd-subject.txt
 MRGD_PATCH_FILE=$OUT_DIR/mrgd-patch.txt
 CMT_PATCH=$OUT_DIR/.patch
 TGT_PATCH=$OUT_DIR/.tgt_patch
@@ -60,6 +61,7 @@ rm $UMRGD_FILE 2> /dev/null
 rm $UMRGD_CIDS 2> /dev/null
 rm $CHRY_PICKS 2> /dev/null
 rm $CONFLICT_CMTS 2> /dev/null
+rm $MRGD_SUBJECT_FILE 2> /dev/null
 rm $MRGD_PATCH_FILE 2> /dev/null
 TARGET_HEAD=$(git log $TARGET_BRANCH --format=%h -1)
 git log $TARGET_BRANCH --oneline > $TARGET_LOG
@@ -74,13 +76,14 @@ do
 	then
 		#echo found $tgt
 		tgt_cid=$(echo $tgt | awk '{print $1}')
-		echo "===================$line =================" >>$MRGD_FILE
-		echo "diff $cid $tgt_cid" >> $MRGD_FILE
+		echo "$cid" >> $MRGD_FILE
+		echo "===================$line =================" >>$MRGD_SUBJECT_FILE
+		echo "diff $cid $tgt_cid" >> $MRGD_SUBJECT_FILE
 		git show -U0 $cid > $CMT_PATCH
 		git show -U0 $tgt_cid > $TGT_PATCH
-		diff -u0 $CMT_PATCH $TGT_PATCH >> $MRGD_FILE
-		echo >>$MRGD_FILE
-		echo >>$MRGD_FILE
+		diff -u0 $CMT_PATCH $TGT_PATCH >> $MRGD_SUBJECT_FILE
+		echo >>$MRGD_SUBJECT_FILE
+		echo >>$MRGD_SUBJECT_FILE
 	else
 		index=$(($index+1));
  		#echo "estuary don't have $line" 
@@ -102,6 +105,7 @@ do
 	then
 		#echo 
 		#echo "$line merge in success."
+		echo "$line" >> $MRGD_FILE
 		echo $line >> $UMRGD_FILE
 		echo "git cherry-pick $line" >> $CHRY_PICKS
 	else
@@ -122,7 +126,8 @@ done < $SORT_UMRGD_CIDS
 git reset --hard $TARGET_HEAD > /dev/null 2>&1
 echo
 echo "======================merged=========================="
-echo "same subject:   $MRGD_FILE"
+echo "all merged commits in $MRGD_FILE"
+echo "same subject:   $MRGD_SUBJECT_FILE"
 echo "different subject:   $MRGD_PATCH_FILE"
 echo
 echo "======================unmerged========================"
