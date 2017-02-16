40dff147b4c133bebabc7255c616cf4c060ea7f6
fengchunsong <chunsongfeng@163.com>
2017-02-16 12:59:04 +0800

    add centos_services_disable.sh
    Signed-off-by: fengchunsong <chunsongfeng@163.com>

---
+++ scripts/centos_services_disable.sh
@@ -0,0 +1,21 @@
+systemctl disable auditd.service                   
+systemctl disable crond.service                    
+systemctl disable dbus.service                     
+systemctl disable firewalld.service                
+systemctl disable getty@tty1.service               
+systemctl disable irqbalance.service               
+systemctl disable network.service                  
+systemctl disable NetworkManager.service           
+systemctl disable polkit.service                   
+systemctl disable postfix.service                  
+systemctl disable rhel-dmesg.service               
+systemctl disable rsyslog.service                  
+systemctl disable serial-getty@ttyS0.service       
+systemctl disable sshd.service                     
+systemctl disable systemd-logind.service           
+systemctl disable systemd-readahead-collect.service
+systemctl disable systemd-readahead-replay.service 
+systemctl disable systemd-user-sessions.service    
+systemctl disable tuned.service                    
+systemctl disable wpa_supplicant.service 
+
